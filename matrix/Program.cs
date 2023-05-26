639da6a2a93ae2c9e9eab3b85cb3cc5d230fd103
murtaza.aslani <murtaza.aslani1@gmail.com>
2023-05-26 23:51:02 +0430

    This code is about matrix

---
+++ matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            // تعریف ماتریس
+            double[,] matrix = new double[,] { { 1, 2 }, { 3, 4 } };
+
+            // محاسبه دیترمینانت
+            double determinant = CalculateDeterminant(matrix);
+
+            // چاپ نتیجه
+            Console.WriteLine("Determinant: " + determinant);
+            Console.ReadLine();
+        }
+
+        static double CalculateDeterminant(double[,] matrix)
+        {
+            double determinant = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            return determinant;
+        }
+    }
+}
